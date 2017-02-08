c260024c87e3f404e12964b65f5f11e0fe195939
pekko1215 <pekko1215@yahoo.co.jp>
2017-02-08 14:02:57 +0900

    test2

--- picfill.py
+++ picfill.py
@@ -1,9 +1,14 @@
+# -*- coding: utf-8 -*-
+
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 
-# read an image
-img = cv2.imread('test.jpg')
-# show image format (basically a 3-d array of pixel color info, in BGR format)
-plt.imshow(img)
-plt.show()
+im = cv2.imread("test.jpg",1)
+#im = cv2.cvtColor(im, cv2.COLOR_GRAY2RGB)
+
+
+print(im.shape)
+#cv2.imshow("Show",im)
+#cv2.waitKey(0)
+#cv2.destroyAllWindows()
